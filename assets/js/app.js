c8dfa21fd7d93faf9e4bc74687fe18e291d9716e
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-06-30 09:03:48 -0700

    added javascript thank you message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,11 @@
 // sanity check
 console.log("app.js linked.");
+
+
+  var clickFunction = function (event) {
+      //do some stuff here
+      alert("Thanks for visiting my site!");
+      window.removeEventListener('click',clickFunction, false );
+
+  };
+  window.addEventListener("click", clickFunction, false);
